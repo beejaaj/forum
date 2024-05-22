6f7d85265569b5df080c181bcab0738b48618f62
MOISES NOGUEIRA SILVA <0031432412027@lti.fatecso.edu.br>
2024-05-22 17:17:43 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function listAllUsers(){
+        //lógica
+        return view('users.listAllUsers');
+    }
+
+    public function listUserByID(){
+
+    }
+
+    public function createUser(){
+
+    }
+}
---
+++ resources/views/users/listAllUsers.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.gpt')
+
+@section('header', 'Listar Todos Usuários')
+
+@section('content')
+
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+        </tr>
+        <tr>
+            <td>Fulano</td>
+            <td>Fulano@email.com</td>
+        </tr>
+    </table>
+
+@endsection
