7223221f84b7666aaa950cfa1306bb240cfe6ece
beejaaj <moises.n.silva.sp2005@gmail.com>
2024-08-21 16:21:14 -0300

    Relação n-n b. posts & users = rating

--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -5,19 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Rate extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'image',
+        'rate',
     ];
 
     public function user(){
         return $this->belongsTo(User::class);
     }
 
-    public function ratings(){
-        return $this->hasMany(Rate::class);
+    public function post(){
+        return $this->belongsTo(Post::class);
     }
 }
