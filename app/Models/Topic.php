15741ee90a31a0dca677815aa5065c35e8dd2046
beejaaj <moises.n.silva.sp2005@gmail.com>
2024-08-21 15:07:53 -0300

    Adicionado os modelos categoria comentario tag etc

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,13 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Topic extends Model
+class Topic extends Post
 {
     use HasFactory;
 
     protected $fillable = [
         'title',
         'description',
-        'status'
+        'status',
     ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Commentary::class);
+    }
 }
