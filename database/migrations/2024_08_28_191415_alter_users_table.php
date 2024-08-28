1ac07cdba3aa43421f66aaad015a69ea7968b89f
beejaaj <moises.n.silva.sp2005@gmail.com>
2024-08-28 16:29:29 -0300

    Migrações e up/down alter_users

---
+++ database/migrations/2024_08_28_191415_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Scheme::table('users', function (Blueprint $table){
+            $table->string('photo');
+            $table->enum('role', ['user', 'moderator', 'admin'])->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('photo');
+        });
+    }
+};
