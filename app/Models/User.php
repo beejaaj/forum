152de3cfc56f86c5d094826ee86c959ea6a5d46a
beejaaj <moises.n.silva.sp2005@gmail.com>
2024-11-27 16:20:22 -0300

    Anotações 27/11/24: Armazenamento de Imagens

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,34 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/*Laravel . Clean Code . MongoDB [?]
+
+VIEW ---
+<form> action="{{route('users.update') }}" method=POST
+decorator: @method('PUT')
+
+<label> class=form-label<foto>
+
+<input type=file>
+--- VIEW
+
+__
+CONTROLLER
+a validação da foto (se ela é png ou não [mimes], por ex. pdf) ocorre no update do UserController
+inputbox==validate(['image'
+
+?
+@error _mostrar mensagem de erro
+
+::Banco de dados não-relacional <-> armazenamento de imagens em base64 .opcional;/não viavel, fora do escopo
+= Nomes de arquivos. As imagens podem ser salvas de modo que um usuario sobrescreva a foto de outro. Como evitar?
+
+Para poder acessar publicamente a pasta storage (public/storage ../images/arquivo.png):
+php artisan storage::link
+
+github/profAndreSouza
+*/
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
